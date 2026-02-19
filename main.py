e0039f99281f59927a8c7309376851251c182341
Rocky324-tech <rockydicky1289@gmail.com>
2026-02-19 12:24:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ api_hash = "97ac27160280bf3ece3c3fb85ae22123"
 
 session_string = "BQJWbioAdeOTcPy1ha-lnt_D1QkWJzHMADFHY65HchvZ_ft08GuIVo9FoCYxCCrhCGWjjCfJv_IXr8m5N6LRv1xeWBtLoywM6fmprUBKAzIN4tSeokjWUDlwzI1j8bj-U6sB0WkVxtH1jiWk2W6MqdKwWdrdSCGz0bAqmF2UFm_gdMy8LR-zIqIF7h90ONYPgY-qfBH8zIQVEP_NXv6fLTr03t8QnsBLbEcfoNrgca5mQ0NwGQcmuuOtO0fMC49-dwd9QWKjAKZAGi2W9Dni4hVtR9_edVotfinm0DdJ7mHFPjvmA16xtlafXV1oWvwmnM4pL_NiERBUF-KoQFQayxCWT2t78wAAAAH5OFoHAA"
 
-sources = [
+# sources (ID)
+source_ids = [
     -1003798031630,
     -1001912679284
 ]
 
-destinations = [
-    -1003793224429,
-    -1003889857801
+# destinations (username)
+destination_usernames = [
+    "t20bapu",
+    "Fan1cricket"
 ]
 
 app = Client(
@@ -24,16 +26,32 @@ app = Client(
     session_string=session_string
 )
 
-@app.on_message(filters.chat(sources))
-async def forward_handler(client, message):
-    print(f"Message detected: {message.id}")
-    for dest in destinations:
+destination_ids = []
+
+async def resolve_destinations():
+    global destination_ids
+    for username in destination_usernames:
+        try:
+            chat = await app.get_chat(username)
+            destination_ids.append(chat.id)
+            print(f"Resolved {username} → {chat.id}")
+        except Exception as e:
+            print(f"Resolve failed {username}: {e}")
+
+@app.on_message(filters.chat(source_ids))
+async def handler(client, message):
+    print("Message detected")
+    for dest in destination_ids:
         try:
             await message.copy(dest)
             print(f"Copied to {dest}")
         except RPCError as e:
-            print(f"Error: {e}")
+            print(e)
 
-print("ULTRA PRO FORWARDER STARTED")
+async def main():
+    await app.start()
+    print("ULTRA PRO AUTO RESOLVE FORWARDER STARTED")
+    await resolve_destinations()
+    await asyncio.Event().wait()
 
-app.run()
+asyncio.run(main())
