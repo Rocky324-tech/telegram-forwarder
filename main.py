3d31a7f1ba222a7723892d9f910ec46c3084f42e
Rocky324-tech <rockydicky1289@gmail.com>
2026-02-19 10:54:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,35 +4,28 @@ from pyrogram import Client, filters, idle
 api_id = 39218730
 api_hash = "97ac27160280bf3ece3c3fb85ae22123"
 
-session_string = "BQJWbioAGnVxisRP9gzo4pyJCz1eHrviCVzATI3ogtiyQLT_1jrzCSlJcmT3qOQEyDef1W9btyk3XANpMXDDmiG0Kx5-KUGWmMr0uv6T_fRA8h_KLb1o2itfDp53QhTZtCjDHzxuJXa6XXWqA4lDFqc0RM6khxw9WWILePiduv9jd0qDK2TByN1xUiDamGDsBfVF9u1xHKrJDnIVSi-fahcpp7FVLDq-KZEfrc7wpt23_Zx9j9dKr2kGdwacyVO6oSUxCt9fjbAkMPPVJ4aXBUEerPw-2Mu50eerD1bvvR_ulqrVwYMZh_FF5BT7xn3cBn2JERqeYCiwYIN4q-idGeJawhTyPAAAAAH5OFoHAA"
+SESSION = "BQJWbioAGnVxisRP9gzo4pyJCz1eHrviCVzATI3ogtiyQLT_1jrzCSlJcmT3qOQEyDef1W9btyk3XANpMXDDmiG0Kx5-KUGWmMr0uv6T_fRA8h_KLb1o2itfDp53QhTZtCjDHzxuJXa6XXWqA4lDFqc0RM6khxw9WWILePiduv9jd0qDK2TByN1xUiDamGDsBfVF9u1xHKrJDnIVSi-fahcpp7FVLDq-KZEfrc7wpt23_Zx9j9dKr2kGdwacyVO6oSUxCt9fjbAkMPPVJ4aXBUEerPw-2Mu50eerD1bvvR_ulqrVwYMZh_FF5BT7xn3cBn2JERqeYCiwYIN4q-idGeJawhTyPAAAAAH5OFoHAA"
 
-SOURCE_CHANNELS = [
-    -1001912679284,
-    -1003798031630
+SOURCE = [
+    -1003798031630,
+    -1001912679284
 ]
 
-DESTINATION_CHANNEL = -1003889857801
+DESTINATION = -1003889857801
 
 app = Client(
-    "forwarder",
+    SESSION,
     api_id=api_id,
-    api_hash=api_hash,
-    session_string=session_string,
-    workers=200,
-    sleep_threshold=0
+    api_hash=api_hash
 )
 
-@app.on_message(filters.chat(SOURCE_CHANNELS))
-async def forward_handler(client, message):
-    try:
-        await message.copy(chat_id=DESTINATION_CHANNEL)
-        print("Forwarded")
-    except Exception as e:
-        print(e)
+@app.on_message(filters.chat(SOURCE))
+async def forward(client, message):
+    await message.copy(DESTINATION)
 
 async def main():
     await app.start()
-    print("🚀 Forwarder running 24/7 🚀")
+    print("Forwarder running...")
     await idle()
 
 asyncio.run(main())
